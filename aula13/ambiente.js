316381e5ba7051975f196c3d64709079d9f05059
Felipe Camargo <felipe93.camargo@gmail.com>
2023-05-02 08:16:32 -0300

    aula13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+
+
+// var c = 1
+// while (c <= 6) {
+//     console.log('Tudo bem?')
+//     c++
+// }
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+
+// Notas:
+//  - O while faz a verificação no início da repetição
+//  - O do while faz a verificação no final
